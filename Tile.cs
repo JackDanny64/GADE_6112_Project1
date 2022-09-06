7cccb73112da20974dab92f6f9046df3287ed0ae
JackDanny64 <110450472+JackDanny64@users.noreply.github.com>
2022-09-06 08:26:44 +0200

    Added the Tile class and its components.

---
+++ Tile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_6112_Project1
+{
+    internal class Tile
+    {
+        private int x;
+        private int y;
+
+        public Tile(int x, int y)
+        {
+            this.x = x;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
