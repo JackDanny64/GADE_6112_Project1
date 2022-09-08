a865c51c99ece0beaf69f13a28b5113028030cb5
Genganas <110450425+Genganas@users.noreply.github.com>
2022-09-08 15:05:13 +0200

    Enemy class

--- Enemy.cs
+++ Enemy.cs
@@ -8,6 +8,10 @@ namespace GADE_6112_Project1
 {
     abstract class Enemy : Character
     {
+        Random random = new Random();
+        protected Enemy(int x, int y) : base(x, y)
+        {
+        }
 
     }
 }
