60480603352e84d3f46fe78f01f1c26f9cf3844f
JackDanny64 <110450472+JackDanny64@users.noreply.github.com>
2022-09-10 11:47:20 +0200

    Fixed enemy class

--- Enemy.cs
+++ Enemy.cs
@@ -9,17 +9,14 @@ namespace GADE_6112_Project1
     abstract class Enemy : Character
     {
         Random random = new Random();
-        protected Enemy(int x, int y) : base(x, y)
+        protected Enemy(int x, int y, int Hp, int MaxHp, int Damage, char Symbol) : base(x, y, MaxHp, Damage) 
         {
+           
         }
-        public int MaxHp = 10;
-        public int damage = 10;
         
-        static Enemy()
+        public override string ToString()
         {
-           EnemysDamage= 10;
-            StartingHp= 10;
-            MaxHp= 10;
+            return $"Enemy at [{X},{Y}] ({damage})";
         }
 
     }
---
+++ Swamp Creature.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_6112_Project1
+{
+     abstract class Swamp_Creature : Enemy
+    {
+        public Swamp_Creature(int x, int y, int Hp, int MaxHp, int damage): base(x,y, Hp, MaxHp, damage)
+        {
+            Hp = 10;
+            damage = 1;
+
+        }
+        public override void ReturnMove()
+        {
+            Random RndDirection = new Random();
+            int RandomMove = RndDirection.Next(0,4);
+            X = 1;
+            Y = 1;
+            
+            if (RandomMove == 1)
+            {
+                Y++;
+            }
+            else if (RandomMove == 2)
+            {
+                X++;
+            }
+            else if(RandomMove == 3)
+            {
+                Y--;
+            }
+            else if(RandomMove == 0)
+            {
+                X++;
+            }
+            
+                
+                    
+            
+
+          
+
+        }
+
+    }
+}
